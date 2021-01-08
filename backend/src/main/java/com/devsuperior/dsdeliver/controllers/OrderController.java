68d17012c124e9865d1c4f73463420bc6c290bd4
amandabuenotozatti <amandabuenotozatti@gmail.com>
2021-01-07 23:00:55 -0300

    POST/ orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,13 +1,17 @@
 //controlador REST para trabalhar com o produto 
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -26,5 +30,12 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll() { //vai encapsular para nós uma resposta de uma requisição HTTP
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list); //metodo que vai criar uma resposta se foi um caso de sucesso 
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert (@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
